5a5ab30ffda7b2020a6b31a9c3d6f611447fc9ff
Charlie Kelly <charliekelly@live.ie>
2015-11-01 23:04:09 +0000

    first commit

---
+++ Iteration1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Iteration1.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult SinglePlayer()
+        {
+            ViewBag.Message = "Single-Player game of Don";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult MultiPlayer()
+        {
+            ViewBag.Message = "Multi-Player game of Don";
+
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Iteration1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Iteration1.Startup))]
+namespace Iteration1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
